087a236e32517a87f4c9daa2007b2b9d282f9dd9
Marek Reid <marekreid@Mareks-MacBook-Pro.local>
2018-11-22 08:41:39 +0100

    first commit

---
+++ precache-manifest.7ee2ac8aad20a3392fc8cca068ae9724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8afa412bba6419ebcad4",
+    "url": "/static/js/main.8afa412b.chunk.js"
+  },
+  {
+    "revision": "4e4098e509fe2e8c0711",
+    "url": "/static/js/1.4e4098e5.chunk.js"
+  },
+  {
+    "revision": "8afa412bba6419ebcad4",
+    "url": "/static/css/main.de5c5c00.chunk.css"
+  },
+  {
+    "revision": "d57fac12c0c52011ae6b5c4cbd478d38",
+    "url": "/index.html"
+  }
+];
